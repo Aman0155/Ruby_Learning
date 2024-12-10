13b28077f205a69609df33e98d89e20370168fc2
aman <amangarg10569@gmail.com>
2024-12-10 14:55:19 +0530

    [Aman] 3. use the redo statement to make them retry entering the value.

--- Ps3.rb
+++ Ps3.rb
@@ -1,3 +1,6 @@
+#Write a Ruby program that prompts the user to input a valid integer between 1 and 10. 
+#If the user inputs an invalid number, 
+#use the redo statement to make them retry entering the value
 loop do
   puts "enter a number"
 num=gets.chomp.to_i
