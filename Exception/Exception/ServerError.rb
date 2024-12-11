000c42f823905b2f44b2e85a40898ac4cfc6e59b
aman <amangarg10569@gmail.com>
2024-12-11 14:45:21 +0530

    [Aman] Add : Server Error  added

---
+++ Exception/Exception/ServerError.rb
@@ -0,0 +1,25 @@
+class RuntimeError < StandardError
+  def initialize(message)
+    super(message)
+  end
+  end
+  i=1
+  begin 
+    rng=Random.new
+    number=rng.rand(1..3)
+    if number!=1
+      raise RuntimeError.new("could not connect")
+    else
+      puts "connected to server"
+    end
+  rescue RuntimeError => e
+    puts "Error : #{e.message}"
+    if i<=3
+      puts "Trying again"
+      i+=1       
+      retry
+    else
+      puts "connection failed"
+      exit 
+    end
+  end
