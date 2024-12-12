310d070061f6df15e3846441f28b54cc3f60832d
aman <amangarg10569@gmail.com>
2024-12-12 15:36:02 +0530

    [Aman]Abstraction shape Hierarchy file added

---
+++ oops/AbstractionShapeHierarchy.rb
@@ -0,0 +1,28 @@
+class Shape
+  def area
+    raise NotImplementedError ,"Subclass not implemented"
+  end
+end
+  
+  class Circle < Shape
+    def initialize(radius)
+      @radius=radius
+    end
+
+   def area
+    puts "Area of circle : #{Math::PI*@radius*@radius}"
+   end
+  end
+  class Rectangle < Shape
+    def initialize(length, width)
+      @length= length
+      @width=width
+    end
+    def area
+      puts "Area of rectangle :#{@length*@width}"
+    end
+  end
+    circle =Circle.new(6)
+    circle.area
+    rectangle =Rectangle.new(10,20)
+    rectangle.area
