e3ae4d694d538463d8cf99bc9f502596bab6b112
aman <amangarg10569@gmail.com>
2024-12-12 23:21:35 +0530

    [Aman] Demonstrated polymorphism by calling send_notification with instances of both EmailMessage and SMSMessage

--- oops/MessagingSystem.rb
+++ oops/MessagingSystem.rb
@@ -1,6 +1,28 @@
 module Message
   def send_message(msg)
-    raise NotImplementedError, " Subclass should have this method "
+    raise NotImplementedError,"Subclasses should have this method"
   end
+  
+  def self.send_notification(object)
+    puts object.send_message
+  end
+end
 
-  def self.se
+class EmailMessage
+  include Message
+  def send_message
+    return "This is an E-Mail"
+  end
+end
+
+class SMSMessage
+  include Message
+  def send_message
+    return "This is a text msg"
+  end
+end
+
+email = EmailMessage.new
+sms = SMSMessage.new
+Message.send_notification(email)
+Message.send_notification(sms)
